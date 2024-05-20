de3edc086d87e8ef518779e1f50527d946429dc6
sstefdev <sstefandev@gmail.com>
2024-05-20 12:17:45 +0200

    feat: added auth-sig

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { config } from "$src/utils";
   import { ViewRequests } from "$lib/index";
   import { requestNetworkStore } from "$stores/requestNetworkStore";
 </script>
 
 <div class="container m-auto w-full">
   <ViewRequests
+    {config}
     wallet={$requestNetworkStore?.walletClient}
     requestNetwork={$requestNetworkStore?.requestNetwork}
   />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [nodePolyfills(), sveltekit()],
+  optimizeDeps: {
+    include: [
+      "js-sha3",
+      "@ethersproject/bignumber",
+      "multiformats",
+      "multiformats/cid",
+    ],
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+      stream: "stream-browserify",
+      assert: "assert",
+      zlib: "browserify-zlib",
+      "multiformats/cid": "multiformats/cid",
+    },
+  },
 });
